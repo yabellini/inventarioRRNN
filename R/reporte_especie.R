58002c4537edc68f1a9cc4b5a99b6b8f3276be79
Yanina Bellini Saibene <yabellini@gmail.com>
2024-11-11 11:17:34 +0100

    proper package call in function

--- R/reporte_especie.R
+++ R/reporte_especie.R
@@ -1,3 +1,19 @@
+#' Lista todas las especies presentes en un censo
+#'
+#' Lista todas las especies presentes en un censo con el calculo
+#' del porcentaje por especie en cada estrato presente. Si la
+#' especie no esta presente en un estrato se presenta NA.
+#'
+#' Si un estrato no tiene presencia de ninguna especie no se
+#' muestra en la tabla.
+#'
+#' @param numero_censo
+#'
+#' @return tabla
+#' @examples
+#' reporte_especie(44)
+#'
+#' @export
 reporte_especie <- function(numero_censo){
 
   data <- inventario_vegetal
@@ -11,8 +27,8 @@ reporte_especie <- function(numero_censo){
   data <- data[data$idInventario %in% inventario, ]
 
   reporte <- data |>
-    select(nombreCientificoEspecie, especie, estrato, porcentajeCobertura) |>
-    pivot_wider(names_from = estrato,
+    dplyr::select(nombreCientificoEspecie, especie, estrato, porcentajeCobertura) |>
+    tidyr::pivot_wider(names_from = estrato,
                 values_from = porcentajeCobertura)
 
   return(reporte)
