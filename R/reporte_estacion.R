3ea7e03501e604a6792e8ac7bea32eac84385eec
Yanina Bellini Saibene <yabellini@gmail.com>
2026-01-18 12:47:01 -0300

    Agrego funcionalidad de indicar la estacion, el censo y calcular el porcentaje ademas de la cantidad.

--- R/reporte_estacion.R
+++ R/reporte_estacion.R
@@ -3,6 +3,7 @@
 #' Listado del total de especies presentes en cada censo
 #' realizado agrupadas por la estación del año en que
 #' crece cada especie.
+#' Opcionalmente muestra porcentajes en lugar de conteos absolutos.
 #'
 #' @return tabla
 #'
@@ -10,19 +11,32 @@
 #' reporte_estacion()
 #'
 #' @export
-reporte_estacion <- function(){
-
-  # TODO: pasarle como parametro un listado de censos.
-  # TODO: pasarle como parametro un listado de estaciones.
-  # data <- inventario_vegetal
-  #
-  # if (!is.null(numero_censo)) {
-  #   inventario <- data[data$censo %in% numero_censo, ]$idInventario
-  # }
+reporte_estacion <- function(censos = NULL, estaciones = NULL, porcentajes = FALSE){
 
   flora <- inventario_flora
 
-#  flora <- flora[flora$idInventario %in% inventario, ]
+  # Filtrar por censo si esta presente
+
+  if (!is.null(censos)) {
+    vegetal <- inventario_vegetal
+    inventarios_validos <- vegetal[vegetal$censo %in% censos, ]$idInventario
+    flora <- flora[flora$idInventario %in% inventarios_validos, ]
+  }
+  # Filtrar por estaciones si esta presente
+
+  if (!is.null(estaciones)) {
+
+    # Validar las estaciones
+    valid_estaciones <- c("Invernal", "Primavero-estival")
+    invalid <- setdiff(estaciones, valid_estaciones)
+
+    if (length(invalid) > 0) {
+      cli::cli_warn(
+        "Estaciones no válidas ignoradas: {cli::col_red(paste(invalid, collapse = ', '))}"
+      )
+    }
+    flora <- flora[flora$estacionalidad %in% estaciones, ]
+  }
 
   # tenemos que contar por estacion la cantidad de especies
   # TODO: mostrarlo con porcentajes
@@ -30,8 +44,14 @@ reporte_estacion <- function(){
   reporte <- flora |>
     dplyr::count(estacionalidad, idInventario) |>
     tidyr::pivot_wider(names_from = estacionalidad,
-                values_from = n)
+                values_from = n,
+                values_fill = 0)
 
+  if (porcentajes) {
+    total_censos <- n_distinct(flora$idInventario)
+    reporte <- reporte |>
+      dplyr::mutate(across(-idInventario, ~ (.x / total_censos) * 100))
+  }
 
   return(reporte)
 
