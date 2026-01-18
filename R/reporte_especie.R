ec01c1bf5c17dc29626e9c78b1f1b2890b83c67e
Yanina Bellini Saibene <yabellini@gmail.com>
2026-01-18 10:36:49 -0300

    Improving documentation and examples

--- R/reporte_especie.R
+++ R/reporte_especie.R
@@ -7,11 +7,22 @@
 #' Si un estrato no tiene presencia de ninguna especie no se
 #' muestra en la tabla.
 #'
-#' @param numero_censo
+#' @param numero_censo Numero entero o vector de numeros que identifican los censos a incluir en el reporte. Deben existir en la columna 'censo' del data frame 'inventario_vegetal'.
 #'
-#' @return tabla
+#' @return Un data frame en formato ancho con las siguientes columnas:
+#' \describe{
+#' \item{nombreCientificoEspecie}{Nombre cientifico de la especie.}
+#' \item{especie}{Nombre comun de la especie.}
+#' \item{<estrato>}{Columnas dinamicas para cada estrato presente en los censos seleccionados, con el porcentaje de cobertura de la especie en ese estrato.}
+#' }
+#' Los valores NA indican que la especie no esta presente en ese estrato.
+#'
 #' @examples
+#' # Reporte de un solo censo
 #' reporte_especie(44)
+#'
+#' # Reporte de varios censos
+#' reporte_especie(c(33, 44, 55))
 #'
 #' @export
 reporte_especie <- function(numero_censo){
