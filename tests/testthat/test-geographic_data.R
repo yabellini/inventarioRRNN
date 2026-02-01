c4b4955d721a88dcbdc8ca108c38975098665ffd
Yanina Bellini Saibene <yabellini@gmail.com>
2026-02-01 19:26:22 -0300

    Testeo

--- tests/testthat/test-geographic_data.R
+++ tests/testthat/test-geographic_data.R
@@ -1,37 +1,37 @@
 test_that("Los datasets geograficos de generales se cargan correctamente", {
   # Test datasets from generales-geoserver
-  data(aeropuertos_la_pampa, package = "inventarioRRNN")
-  expect_s3_class(aeropuertos_la_pampa, "sf")
-  expect_equal(nrow(aeropuertos_la_pampa), 2)
-  
+  data(aeropuertos, package = "inventarioRRNN")
+  expect_s3_class(aeropuertos, "sf")
+  expect_equal(nrow(aeropuertos), 2)
+
   data(microregion_la_pampa, package = "inventarioRRNN")
-  expect_s3_class(microregion_la_pampa, "sf")
-  expect_equal(nrow(microregion_la_pampa), 10)
-  
+  expect_s3_class(microregion, "sf")
+  expect_equal(nrow(microregion), 10)
+
   data(suelos_la_pampa, package = "inventarioRRNN")
-  expect_s3_class(suelos_la_pampa, "sf")
-  expect_equal(nrow(suelos_la_pampa), 261)
+  expect_s3_class(suelos, "sf")
+  expect_equal(nrow(suelos), 261)
 })
 
 test_that("Los datasets geograficos climaticos se cargan correctamente", {
   # Test datasets from variables-climaticas-geoserver
-  data(precipitaciones_anuales_la_pampa, package = "inventarioRRNN")
-  expect_s3_class(precipitaciones_anuales_la_pampa, "sf")
-  expect_equal(nrow(precipitaciones_anuales_la_pampa), 5)
-  
+  data(precipitaciones_anuales, package = "inventarioRRNN")
+  expect_s3_class(precipitaciones_anuales, "sf")
+  expect_equal(nrow(precipitaciones_anuales), 5)
+
   data(temperatura_media_la_pampa, package = "inventarioRRNN")
-  expect_s3_class(temperatura_media_la_pampa, "sf")
-  expect_equal(nrow(temperatura_media_la_pampa), 4)
-  
+  expect_s3_class(temperatura_media, "sf")
+  expect_equal(nrow(temperatura_media), 4)
+
   data(horas_frio_anuales_la_pampa, package = "inventarioRRNN")
-  expect_s3_class(horas_frio_anuales_la_pampa, "sf")
-  expect_equal(nrow(horas_frio_anuales_la_pampa), 4)
+  expect_s3_class(horas_frio_anuales, "sf")
+  expect_equal(nrow(horas_frio_anuales), 4)
 })
 
 test_that("Los datasets geograficos tienen columnas geometry", {
   data(aeropuertos_la_pampa, package = "inventarioRRNN")
-  expect_true("geometry" %in% names(aeropuertos_la_pampa))
-  
+  expect_true("geometry" %in% names(aeropuertos))
+
   data(precipitaciones_anuales_la_pampa, package = "inventarioRRNN")
-  expect_true("geometry" %in% names(precipitaciones_anuales_la_pampa))
+  expect_true("geometry" %in% names(precipitaciones_anuales))
 })
